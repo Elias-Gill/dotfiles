8d6aea86132d9c5c734404472aaaadfb18152ee2
Elias Gill <eliasgill42@gmail.com>
2025-11-05 10:44:31 -0300

    fix: configuracion de mango

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,7 +1,5 @@
 # monitores
-bash ~/.config/mango/auto_monitor.sh
-# bar
-waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+bash ~/.config/mango/auto_monitor.sh &
 # systray
 nm-applet >/dev/null 2>&1 &
 blueman-applet >/dev/null 2>&1 &
@@ -9,5 +7,13 @@ blueman-applet >/dev/null 2>&1 &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
 # notificaciones
 swaync &
+# Autosuspend y demas
+swayidle -w \
+    timeout 600 'swaylock --image "$(cat ~/.config/swaybg)"' \
+    timeout 900 'systemctl suspend' \
+    timeout 3600 'systemctl hibernate' \
+    before-sleep 'swaylock --image "$(cat ~/.config/swaybg)"' &
+# bar
+waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 # fondo de pantalla
 swaybg -i $(cat ~/.config/swaybg) >/dev/null 2>&1 &
