e88468fd1548b4550c4bbaebe5c86e6a3a0dd63c
Elias Gill <eliasgill42@gmail.com>
2026-01-13 23:43:36 -0300

    fix: yazi migration

--- .config/yazi/sxiv_open.bash
+++ .config/yazi/sxiv_open.bash
@@ -7,7 +7,7 @@ base="$(basename -- "$selected")"
 
 mapfile -t images < <(
   find "$dir" -maxdepth 1 -type f \
-    | grep -Ei '\.(jpg|jpeg|png|webp|gif|bmp)$' \
+    | grep -Ei '\.(jpg|gif|jpeg|png|webp|bmp)$' \
     | sed "s|^$dir/||" \
     | sort -V -f
 )
@@ -25,4 +25,4 @@ for img in "${images[@]}"; do
   args+=("$dir/$img")
 done
 
-exec sxiv -n "$start_index" "${args[@]}"
+exec nsxiv -a -n "$start_index" "${args[@]}"
